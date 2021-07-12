ca8b8c6bfe18c8c845f0988c44517ba48f6f64b5
Dev_Ose <osemum@hardcorebiometric.com>
2021-07-12 14:36:40 +0100

    Added classes for dark theme mode

--- shared/components/sidebar/SideBarItem.js
+++ shared/components/sidebar/SideBarItem.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function SideBarItem({src, Icon, title}) {
     return (
-        <div className="flex items-center space-x-2 p-4 hover:bg-gray-200 rounded-xl cursor-pointer">
+        <div className="flex items-center space-x-2 p-4 hover:bg-gray-200 dark:hover:bg-gray-500 rounded-xl cursor-pointer">
             {src && (
                 <Image
                     className="rounded-full"
@@ -14,8 +14,8 @@ function SideBarItem({src, Icon, title}) {
                 /> 
             )}
 
-            {Icon && <Icon className="h-8 w-8 text-blue-500" />}
-            <p className="hidden sm:inline-flex font-medium">{title}</p>
+            {Icon && <Icon className="h-8 w-8 text-blue-500 dark:text-blue-200" />}
+            <p className="hidden sm:inline-flex font-medium dark:text-white">{title}</p>
         </div>
     )
 }
